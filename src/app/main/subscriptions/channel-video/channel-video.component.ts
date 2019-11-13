9cb0d78cdbcb958c77a6d8b7e199cdfb2f37ec0a
dshapoval <dshapoval.epay@gmail.com>
2019-11-13 21:06:46 +0200

    create channel video page

--- src/app/main/subscriptions/channel-video/channel-video.component.ts
+++ src/app/main/subscriptions/channel-video/channel-video.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { YoutubeApiService } from '../../../shared/services/youtube-api.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-channel-video',
@@ -10,12 +11,14 @@ import { YoutubeApiService } from '../../../shared/services/youtube-api.service'
 export class ChannelVideoComponent implements OnInit {
 
   public channelId: string;
-  public maxResult: string = '50';
+  public maxResult: string = '10';
   public channelVideos: Array<any>;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private youtubeApiService: YoutubeApiService
+    private youtubeApiService: YoutubeApiService,
+    private sanitizer: DomSanitizer,
+
   ) {
 
   }
@@ -26,6 +29,12 @@ export class ChannelVideoComponent implements OnInit {
     console.log(this.channelId);
     this.getSubscriptVideo(this.channelId, this.maxResult);
   }
+
+  public sanitizedUrl(id): SafeResourceUrl|string {
+    const url = `https://www.youtube.com/embed/${id}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
   public getSubscriptVideo(channelId, maxResult): any {
     this.youtubeApiService.getVideoByChannelId(channelId, maxResult)
       .subscribe(
